08d3d51ef19f2ca2b2f2616e9997d82ff077906a
Rostyslav Meleshko <meleshkorostyslav@gmail.com>
2021-03-03 18:31:45 +0200

    Updates

---
+++ precache-manifest.37d0160ecebd8b9e1709a4786d4fefa2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1af86a25f7ccaa89e9ba84f41e69c7c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1b82f20aee3b446fa527",
+    "url": "./static/css/2.87a9c7a2.chunk.css"
+  },
+  {
+    "revision": "203c7a2c4b5cd39622d6",
+    "url": "./static/css/main.b4ab70fe.chunk.css"
+  },
+  {
+    "revision": "1b82f20aee3b446fa527",
+    "url": "./static/js/2.b9115898.chunk.js"
+  },
+  {
+    "revision": "ce15a0ffcfe8b91e4f219c1d7bb8c5ef",
+    "url": "./static/js/2.b9115898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "203c7a2c4b5cd39622d6",
+    "url": "./static/js/main.dc7f0c59.chunk.js"
+  },
+  {
+    "revision": "c7ae2510e9f54903a2ed",
+    "url": "./static/js/runtime-main.b51b247a.js"
+  }
+]);
